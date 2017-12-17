f47d1549e9594f207374da6d3a52c6ff9934c897
slowlyric89 <arudak@hotmail.com>
2017-12-17 20:49:54 +0100

    Update shell script

--- src/main/shell/run.sh
+++ src/main/shell/run.sh
@@ -2,4 +2,4 @@
 
 export BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$SPARK_HOME/bin/spark-submit --class lyric.slow.spark.SparkPi --master yarn-client $BASE/../../../target/scala-2.11/test-scala_2.11-0.5.jar
+$SPARK_HOME/bin/spark-submit --class lyric.slow.spark.SparkPi --master yarn $BASE/../../../target/scala-2.11/test-scala_2.11-0.5.jar
